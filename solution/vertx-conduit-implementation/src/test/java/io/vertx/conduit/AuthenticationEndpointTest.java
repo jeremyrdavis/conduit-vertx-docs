099c7f8459ea69d97c6c6b762a2724e18485f4b8
jeremyrdavis <jeremyrdavis@jeremyrdavis.com>
2018-10-07 23:29:10 -0400

    added migrations

---
+++ solution/vertx-conduit-implementation/src/test/java/io/vertx/conduit/AuthenticationEndpointTest.java
@@ -0,0 +1,41 @@
+package io.vertx.conduit;
+
+import com.mchange.v2.c3p0.test.TestConnectionCustomizer;
+import io.vertx.core.Vertx;
+import io.vertx.core.json.JsonObject;
+import io.vertx.ext.web.client.WebClient;
+import io.vertx.junit5.VertxExtension;
+import io.vertx.junit5.VertxTestContext;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@DisplayName("Authentication Endpoint Tests")
+@ExtendWith(VertxExtension.class)
+public class AuthenticationEndpointTest {
+
+  @Test
+  public void testAuthentication(Vertx vertx, VertxTestContext testContext){
+    vertx.deployVerticle(new MainVerticle(), testContext.succeeding(id -> {
+      WebClient webClient = WebClient.create(vertx);
+      webClient.post(8080, "localhost", "/api/users/login")
+        .sendJsonObject(new JsonObject()
+        .put("user", new JsonObject()
+          .put("email", "jake@jake.jake")
+          .put("password", "jakejake")
+        ), response -> testContext.verify(() -> {
+        JsonObject user = response.result().bodyAsJsonObject().getJsonObject("user");
+        assertEquals(200, response.result().statusCode());
+        assertEquals("jake@jake.jake", user.getString("email"));
+        assertEquals("jakejake", user.getString("password"));
+        assertEquals("jwt.token.here", user.getString("token"));
+        assertEquals("jake", user.getString("username"));
+        assertEquals("I work at statefarm", user.getString("bio"));
+        assertEquals("", user.getString("image"));
+        testContext.completeNow();
+      }));
+    }));
+  }
+}
