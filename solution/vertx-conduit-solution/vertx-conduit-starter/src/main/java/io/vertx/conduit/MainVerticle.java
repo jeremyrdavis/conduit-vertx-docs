2e8eb3f02003f7a6d83c8e9ac25af600b70027b3
jeremyrdavis <jeremyrdavis@jeremyrdavis.com>
2018-10-11 11:30:30 -0400

    Router is implemented and test passing

--- solution/vertx-conduit-solution/vertx-conduit-starter/src/main/java/io/vertx/conduit/MainVerticle.java
+++ solution/vertx-conduit-solution/vertx-conduit-starter/src/main/java/io/vertx/conduit/MainVerticle.java
@@ -1,14 +1,30 @@
 package io.vertx.conduit;
 
 import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Future;
+import io.vertx.core.http.HttpServerResponse;
+import io.vertx.ext.web.Router;
+import io.vertx.ext.web.RoutingContext;
 
 public class MainVerticle extends AbstractVerticle {
 
   @Override
-  public void start() {
+  public void start(Future<Void> startFuture) {
+
+    Router router = Router.router(vertx);
+    router.route("/").handler(this::indexHandler);
+
     vertx.createHttpServer()
         .requestHandler(req -> req.response().end("Hello, Conduit!"))
         .listen(8080);
+    startFuture.complete();
+  }
+
+  private void indexHandler(RoutingContext routingContext) {
+    HttpServerResponse response = routingContext.response();
+    response
+      .putHeader("Content-Type", "text/html")
+      .end("Hello Conduit!");
   }
 
 }
