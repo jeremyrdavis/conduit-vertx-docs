a8b3a68886ac5e89e2056c157ff2d99abf61540d
jeremyrdavis <jeremyrdavis@jeremyrdavis.com>
2018-10-11 17:20:43 -0400

    stubbed out response

--- solution/vertx-conduit-solution/vertx-conduit-starter/src/main/java/io/vertx/conduit/MainVerticle.java
+++ solution/vertx-conduit-solution/vertx-conduit-starter/src/main/java/io/vertx/conduit/MainVerticle.java
@@ -3,21 +3,53 @@ package io.vertx.conduit;
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Future;
 import io.vertx.core.http.HttpServerResponse;
+import io.vertx.core.json.JsonObject;
 import io.vertx.ext.web.Router;
 import io.vertx.ext.web.RoutingContext;
+import io.vertx.ext.web.handler.BodyHandler;
 
 public class MainVerticle extends AbstractVerticle {
 
   @Override
   public void start(Future<Void> startFuture) {
 
-    Router router = Router.router(vertx);
-    router.route("/").handler(this::indexHandler);
+    Router baseRouter = Router.router(vertx);
+    baseRouter.route("/").handler(this::indexHandler);
 
-    vertx.createHttpServer()
-        .requestHandler(req -> req.response().end("Hello, Conduit!"))
-        .listen(8080);
-    startFuture.complete();
+    Router apiRouter = Router.router(vertx);
+    apiRouter.route("/*").handler(BodyHandler.create());
+    apiRouter.post("/users").handler(this::registrationHandler);
+
+    baseRouter.mountSubRouter("/api", apiRouter);
+
+    vertx
+      .createHttpServer()
+      .requestHandler(baseRouter::accept)
+      .listen(8080, result -> {
+        if (result.succeeded()) {
+          startFuture.complete();
+        }else {
+          startFuture.fail(result.cause());
+        }
+      });
+  }
+
+  private void registrationHandler(RoutingContext routingContext) {
+
+    JsonObject user = routingContext.getBodyAsJson().getJsonObject("user");
+
+    JsonObject returnValue = new JsonObject()
+      .put("user", new JsonObject()
+        .put("email", user.getString("email"))
+        .put("password", user.getString("password"))
+        .put("token", "")
+        .put("username", user.getString("username"))
+        .put("bio", "")
+        .put("image", ""));
+    routingContext.response()
+      .setStatusCode(201)
+      .putHeader("Content-Type", "application/json; charset=utf-8")
+      .end(returnValue.encodePrettily());
   }
 
   private void indexHandler(RoutingContext routingContext) {
