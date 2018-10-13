051aa26da01cdeba9057ca6b8aecf14511fcb7a9
jeremyrdavis <jeremyrdavis@jeremyrdavis.com>
2018-10-13 09:50:57 -0400

    progress on db

--- solution/vertx-conduit-solution/vertx-conduit-starter/src/main/java/io/vertx/conduit/SQLQueries.java
+++
@@ -1,8 +0,0 @@
-package io.vertx.conduit;
-
-public class SQLQueries {
-
-  public static final String SQL_REGISTER_USER = "insert into USER (\"username\",\"email\",\"bio\",\"password\") values (?, ?, ?, ?);";
-  public static final String SQL_SELECT_USER_BY_EMAIL = "select * from USER where \"email\" = ?";
-
-}
